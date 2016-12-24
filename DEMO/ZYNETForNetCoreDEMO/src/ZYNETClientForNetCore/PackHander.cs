c10ce46b2ef376c6fa0eecaa2b74a68ce71af684
luyikk <547386448@qq.com>
2016-12-24 21:35:21 +0800

    POST DOTNETCORE DEMO

---
+++ DEMO/ZYNETForNetCoreDEMO/src/ZYNETClientForNetCore/PackHander.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZYNet.CloudSystem;
+using ZYNet.CloudSystem.Client;
+using ZYNet.CloudSystem.Frame;
+using System.Net.Http;
+
+namespace ZYNETClientForNetCore
+{
+
+    /// <summary>
+    /// 客户端包处理器
+    /// </summary>
+    public class PackHander
+    {
+        [MethodRun(2001)]
+        public async Task<ReturnResult> DownHtml(AsyncCalls async, string url)
+        {
+            HttpClient  client = new HttpClient();
+            byte[] html = await client.GetByteArrayAsync(url);
+
+            return async.RET(html);
+        }
+
+        [MethodRun(3001)]
+        public void Message(CloudClient client, string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+
+        [MethodRun(2500)]
+        public async Task<ReturnResult> TestRec(AsyncCalls async, int count)
+        {
+            count--;
+            if (count > 1)
+            {
+                var x = (await async.CR(2500, count))?[0]?.Value<int>();
+
+                if (x != null && x.HasValue)
+                {
+                    count = x.Value;
+                }
+            }
+
+            return async.RET(count);
+        }
+    }
+}
---
+++ DEMO/ZYNETForNetCoreDEMO/src/ZYNETClientForNetCore/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZYNet.CloudSystem;
+using ZYNet.CloudSystem.Client;
+using ZYNet.CloudSystem.Frame;
+using ZYNet.CloudSystem.SockClient;
+
+namespace ZYNETClientForNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            LogAction.LogOut += LogAction_LogOut;
+            CloudClient client = new CloudClient(new SocketClient(), 500000, 1024 * 1024); //最大数据包能够接收 1M
+            PackHander tmp = new PackHander();
+            client.Install(tmp);
+            client.Disconnect += Client_Disconnect;
+
+            if (client.Connect("127.0.0.1", 2285))
+            {
+                ZYSync Sync = client.Sync;
+
+                var res = Sync.CR(1000, "AAA", "BBB")?[0]?.Value<bool>();
+
+                if (res != null && res == true)
+                {
+
+                    var html = Sync.CR(2001, "http://www.baidu.com")?[0]?.Value<string>();
+                    if (html != null)
+                    {
+                        Console.WriteLine("BaiduHtml:" + html.Length);
+
+                        var time = Sync.CR(2002)?.First?.Value<DateTime>();
+
+                        Console.WriteLine("ServerTime:" + time);
+
+                        Sync.CV(2003, "123123");
+
+                        var x = Sync.CR(2001, "http://www.qq.com");
+
+                        Console.WriteLine("QQHtml:" + x.First.Value<string>().Length);
+
+                        System.Diagnostics.Stopwatch stop = new System.Diagnostics.Stopwatch();
+                        stop.Start();
+                        var rec = Sync.CR(2500, 10000)?.First?.Value<int>();
+                        stop.Stop();
+                        if (rec != null)
+                        {
+                            Console.WriteLine("Rec:{0} time:{1} MS", rec.Value, stop.ElapsedMilliseconds);
+                        }
+                    }
+                }
+
+                Console.ReadLine();
+            }
+
+        }
+
+        private static void LogAction_LogOut(string msg, LogType type)
+        {
+            Console.WriteLine(msg);
+        }
+
+        private static void Client_Disconnect(string obj)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
